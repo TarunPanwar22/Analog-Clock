67cfc92d0da232234e2a7664736c5e6e6ff14f67
Tarun Panwar <tarun.panwar182@gmail.com>
2022-08-29 16:17:29 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+    // console.log("check")
+    d = new Date(); 
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2 ; 
+    mrotation = 6 * mtime ;
+    srotation = 6*stime; 
+    // console.log(srotation);
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000 )
